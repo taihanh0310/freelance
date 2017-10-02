969abbb8fb2a86950ff1d6ec1afd86243346b83f
Hanh Nguyen <hanh.nguyentai@nashtechglobal.com>
2017-10-02 16:39:37 +0700

    shop

---
+++ shopThong/resources/views/layouts/default.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('page_title', setting('site.title') . " - " . setting('site.description'))</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}"/>
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ voyager_asset('images/logo-icon.png') }}" type="image/x-icon">
+
+
+
+    <!-- App CSS -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    @yield('css')
+
+    @yield('head')
+</head>
+
+<body>
+
+<div id="voyager-loader">
+    <?php $admin_loader_img = Voyager::setting('admin.loader', ''); ?>
+    @if($admin_loader_img == '')
+        <img src="{{ voyager_asset('images/logo-icon.png') }}" alt="Voyager Loader">
+    @else
+        <img src="{{ Voyager::image($admin_loader_img) }}" alt="Voyager Loader">
+    @endif
+</div>
+
+<div class="app-container">
+    <div class="fadetoblack visible-xs"></div>
+    <div class="row content-container">
+        @include('voyager::dashboard.navbar')
+        @include('voyager::dashboard.sidebar')
+        <script>
+            (function(){
+                    var appContainer = document.querySelector('.app-container'),
+                        sidebar = appContainer.querySelector('.side-menu'),
+                        navbar = appContainer.querySelector('nav.navbar.navbar-top'),
+                        loader = document.getElementById('voyager-loader'),
+                        hamburgerMenu = document.querySelector('.hamburger'),
+                        sidebarTransition = sidebar.style.transition,
+                        navbarTransition = navbar.style.transition,
+                        containerTransition = appContainer.style.transition;
+
+                    sidebar.style.WebkitTransition = sidebar.style.MozTransition = sidebar.style.transition =
+                    appContainer.style.WebkitTransition = appContainer.style.MozTransition = appContainer.style.transition =
+                    navbar.style.WebkitTransition = navbar.style.MozTransition = navbar.style.transition = 'none';
+
+                    if (window.localStorage && window.localStorage['voyager.stickySidebar'] == 'true') {
+                        appContainer.className += ' expanded no-animation';
+                        loader.style.left = (sidebar.clientWidth/2)+'px';
+                        hamburgerMenu.className += ' is-active no-animation';
+                    }
+
+                   navbar.style.WebkitTransition = navbar.style.MozTransition = navbar.style.transition = navbarTransition;
+                   sidebar.style.WebkitTransition = sidebar.style.MozTransition = sidebar.style.transition = sidebarTransition;
+                   appContainer.style.WebkitTransition = appContainer.style.MozTransition = appContainer.style.transition = containerTransition;
+            })();
+        </script>
+        <!-- Main Content -->
+        <div class="container-fluid">
+            <div class="side-body padding-top">
+                @yield('page_header')
+                <div id="voyager-notifications"></div>
+                
+            </div>
+        </div>
+    </div>
+    <!-- Page Content -->
+    <div class="container">
+        @yield('content')
+    </div>
+    <!-- end Page content -->
+</div>
+@include('voyager::partials.app-footer')
+
+<!-- Javascript Libs -->
+
+
+<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+
+@yield('javascript')
+
+</body>
+</html>
