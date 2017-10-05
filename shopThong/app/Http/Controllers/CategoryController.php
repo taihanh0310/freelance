fa9ace11d08d186f2f0482ca83c1f6c10fb46a94
Hanh Nguyen <taihanh0310@gmail.com>
2017-10-05 20:56:58 +0700

    category

--- shopThong/app/Http/Controllers/CategoryController.php
+++ shopThong/app/Http/Controllers/CategoryController.php
@@ -8,11 +8,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\SiteController;
+use Illuminate\Http\Request;
+use TCG\Voyager\Models\Category;
+
 /**
  * Description of CategoryController
  *
  * @author hanhnguyen
  */
-class CategoryController {
-    //put your code here
+class CategoryController extends SiteController {
+
+    public $categorySideBars;
+
+    public function __construct() {
+        $this->categorySideBars = Category::with('posts')->whereIn('id', [3, 4, 5, 6, 7, 8, 9, 10])->get();
+    }
+
+    public function index($slug) {
+        $categorySideBars = $this->categorySideBars;
+        
+        $category = Category::with(['posts' => function($queryPost) {
+                        $queryPost->where('status', 'PUBLISHED')->get();
+                    }])->where('slug', $slug)->firstOrFail();
+        return view('pages.category', compact('category', 'categorySideBars'));
+    }
+
 }
--- shopThong/resources/views/pages/category.blade.php
+++ shopThong/resources/views/pages/category.blade.php
@@ -1,8 +1,30 @@
-<?php
+@extends('layouts.default')
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+@section('content')
+    
+    <!-- Page Heading/Breadcrumbs -->
+    @component('partials.page_heading')
+        Danh sach san pham
+    @endcomponent
 
+    @component('partials.breadcrumb')
+        @slot('route_parent')
+            {{route('home.index')}}
+        @endslot
+        @slot('parent_name')
+            Trang chu
+        @endslot
+        @slot('child_name')
+            {{$category->name}}
+        @endslot
+    @endcomponent
+    
+    <div class="row">
+        <div class="col-sm-3">
+            @includeIf('includes.sidebar', ['sidebars' => $categorySideBars])
+        </div>
+        <div class="col-sm-9">
+            @includeIf('includes.product_2_column', ['category' => $category])
+        </div>
+    </div>
+@stop
