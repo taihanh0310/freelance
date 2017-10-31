ced3dc58acd67345f3d9e40180b893990d34668b
Hanh Nguyen <hanh.nguyentai@nashtechglobal.com>
2017-10-31 10:04:01 +0700

    updated

--- shopThong/resources/views/includes/sidebar.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="list-group">
-    @foreach($sidebars as $category)
-        <a href="{{route('danh_muc_bai_viet', ['category_slug' => $category->slug])}}" class="list-group-item {{ Request::fullUrl() == route('danh_muc_bai_viet', ['category_slug' => $category->slug]) ? 'active' : '' }}" >
-            {{$category->name}}
-            <span class="badge badge-default badge-pill">{{count($category->posts)}}</span>
-        </a>
-    @endforeach
-</div>
