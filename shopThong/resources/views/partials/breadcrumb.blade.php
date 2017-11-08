621d5e72fb4f94068e35cad2041a3e6f877b04da
Hanh Nguyen Tai <taihanh0310@gmail.com>
2017-11-08 21:38:49 +0700

    remove some file errors

--- shopThong/resources/views/partials/breadcrumb.blade.php
+++
@@ -1,6 +0,0 @@
-<ol class="breadcrumb">
-    <li class="breadcrumb-item">
-      <a href="{{$route_parent}}">{{$parent_name}}</a>
-    </li>
-    <li class="breadcrumb-item active">{{$child_name}}</li>
-  </ol>
